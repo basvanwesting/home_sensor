925463f028db35c464f5cc233be54d4de37121e8
basvanwesting <basvanwesting@hotmail.com>
2020-10-14 13:11:04 +0200

    Initial commit, clean nerves.new

---
+++ lib/home_sensor.ex
@@ -0,0 +1,18 @@
+defmodule HomeSensor do
+  @moduledoc """
+  Documentation for HomeSensor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HomeSensor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/home_sensor_test.exs
@@ -0,0 +1,8 @@
+defmodule HomeSensorTest do
+  use ExUnit.Case
+  doctest HomeSensor
+
+  test "greets the world" do
+    assert HomeSensor.hello() == :world
+  end
+end
