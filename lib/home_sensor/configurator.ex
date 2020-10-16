56153aff2bd5a5a6d0d605a6172b8af5299558e0
basvanwesting <basvanwesting@hotmail.com>
2020-10-16 14:23:31 +0200

    Copy full poser Configurator approach with cacerts

--- lib/home_sensor/configurator.ex
+++ lib/home_sensor/configurator.ex
@@ -2,22 +2,55 @@ defmodule HomeSensor.Configurator do
   @behaviour NervesHubLink.Configurator
 
   require Logger
+  alias NervesHubLink.Certificate
 
   @impl true
   def build(config) do
-    Logger.info("NervesHubLink config: #{inspect config}")
-
     priv_dir = Application.app_dir(:home_sensor, "priv")
-    certfile = Path.join(priv_dir, "9794-cert.pem")
-    keyfile = Path.join(priv_dir, "9794-key.pem")
-    File.exists?(certfile) || raise "certfile cannot by found: #{certfile}"
-    File.exists?(keyfile)  || raise "keyfile cannot by found: #{keyfile}"
+    certfile = Path.join(priv_dir, "9794.cert")
+    keyfile = Path.join(priv_dir, "9794.key")
 
+    signer =
+      Path.join(priv_dir, "nerveskey_prod_signer1.cert")
+      |> File.read!()
+
+    update_config(config, certfile, keyfile, signer)
+  end
+
+  defp build_cacerts(signer) do
+    signer_der = Certificate.pem_to_der(signer)
+
+    [signer_der | cacerts()]
+  end
+
+  defp update_config(config, certfile, keyfile, signer) do
     ssl =
       config.ssl
       |> Keyword.put(:certfile, certfile)
       |> Keyword.put(:keyfile, keyfile)
+      |> Keyword.put(:cacerts, build_cacerts(signer))
 
-    %{config | ssl: ssl}
+    socket = Keyword.put(config.socket, :reconnect_interval, 15000)
+
+    Logger.info("NervesHubLink config.ssl: #{inspect ssl}")
+
+    %{config | socket: socket, ssl: ssl}
+  end
+
+  defp cacerts() do
+    case Application.get_env(:nerves_hub_link, :ca_certs) do
+      [] = list ->
+        list
+        |> Path.join("*")
+        |> Path.wildcard()
+        |> Enum.map(&to_der/1)
+      nil ->
+        []
+    end
+  end
+
+  defp to_der(file) do
+    File.read!(file)
+    |> Certificate.pem_to_der()
   end
 end
